c419cdb077609865993371a6c74ebf68e3c1d023
satyammishra2594s <satyammishra2594@gmail.com>
2026-09-01 14:09:07 +0530

    Connect portfolio scene to App Router

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,10 @@
-export default function Page() { return <main>Loading...</main> }
+'use client';
+
+import dynamic from 'next/dynamic';
+import Overlay from '../components/Overlay';
+
+const World = dynamic(() => import('../components/World'), { ssr: false });
+
+export default function Home() {
+  return <div className="relative min-h-screen bg-[#030305]"><World /><Overlay /></div>;
+}
