182a65e9e36cc17c4c01172d06c7c4838bc5e99d
satyammishra2594s <satyammishra2594@gmail.com>
2026-09-01 13:49:37 +0530

    Delete lib directory

--- lib/store.ts
+++
@@ -1,3 +0,0 @@
-import {create} from 'zustand';
-interface State{progress:number;section:number;loaded:boolean;reduced:boolean;setProgress:(v:number)=>void;setSection:(v:number)=>void;setLoaded:(v:boolean)=>void;setReduced:(v:boolean)=>void}
-export const useWorldStore=create<State>((set)=>({progress:0,section:0,loaded:false,reduced:false,setProgress:(progress)=>set({progress}),setSection:(section)=>set({section}),setLoaded:(loaded)=>set({loaded}),setReduced:(reduced)=>set({reduced})}));
