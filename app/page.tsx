f11c08d27d424ed3382aa56643013994016fcea6
satyammishra2594s <satyammishra2594@gmail.com>
2026-09-01 13:49:03 +0530

    Delete app directory

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-import dynamic from 'next/dynamic';
-const World=dynamic(()=>import('../components/World'),{ssr:false});
-import Overlay from '../components/Overlay';
-import {useEffect} from 'react';
-import {useWorldStore} from '../lib/store';
-export default function Home(){const setReduced=useWorldStore(s=>s.setReduced);useEffect(()=>{setReduced(matchMedia('(prefers-reduced-motion: reduce)').matches)},[setReduced]);return <div className="relative min-h-screen bg-[#030305]"><World/><Overlay/></div>}
